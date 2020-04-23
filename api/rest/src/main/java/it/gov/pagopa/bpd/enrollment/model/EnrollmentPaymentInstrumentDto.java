08587764cb63999e6cf9598322de08dbffcb3ca0
rodoviti <rodolfo.viti@everis.com>
2020-04-23 15:38:47 +0200

    fix pom

--- api/rest/src/main/java/it/gov/pagopa/bpd/enrollment/model/EnrollmentPaymentInstrumentDto.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/enrollment/model/EnrollmentPaymentInstrumentDto.java
@@ -1,14 +1,14 @@
 package it.gov.pagopa.bpd.enrollment.model;
 
+import it.gov.pagopa.bpd.common.model.validation.FutureOrPresentWithTolerance;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 
 @Data
 public class EnrollmentPaymentInstrumentDto {
 
-    @NotNull
+    @FutureOrPresentWithTolerance(tolerance = 60)
     private OffsetDateTime activationDate;
 
 }
